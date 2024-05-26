fea9ec406f5fabfb67b853827de5eacdfb3528cd
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-27 06:50:59 +0700

    js lab 83

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+let str = "javascript";
+
+function ucFirst(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+}
+
+console.log(ucFirst(str));
